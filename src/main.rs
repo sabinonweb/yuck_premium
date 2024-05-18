7b4c9a86de968e27a2ac8c61a4bcb7002e0cdbe6
Sabin <sabinonweb7@gmail.com>
2024-05-18 20:03:47 +0545

    [Prelim: Sync Download]

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,75 @@
+use reqwest::header::{HeaderValue, CONTENT_LENGTH, RANGE};
 use std::fs::File;
-use std::io::Read;
-use reqwest::Client;
+use reqwest::StatusCode;
+use std::str::FromStr;
 
-#[tokio::main]
-async fn main() -> Result<(), String> {
-    let paste_api = "https://paste.rs";
-    let mut file = File::open("/Users/sabinonweb/Documents/Projects/downloader/src/message.txt").unwrap();
+#[derive(Debug)]
+struct PartialRangeIter {
+    start: u64,
+    end: u64,
+    buffer: u32,
+}
+
+impl PartialRangeIter {
+    fn new(start: u64, end: u64, buffer: u32) -> Result<Self, String> {
+        if buffer == 0 {
+            Err(format!("Buffer must be greater than 0"))
+        } else {
+            Ok(PartialRangeIter {
+                start,
+                end,
+                buffer,
+            })
+        }
+    }
+}
+
+impl Iterator for PartialRangeIter {
+    type Item = HeaderValue;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start > self.end {
+            None
+        } else {
+            let prev_start = self.start;
+            self.start += std::cmp::min(self.buffer as u64, self.end - self.start + 1);
+            Some(HeaderValue::from_str(&format!("bytes={}-{}", prev_start, self.start - 1)).expect("Expected &str, found String"))
+        }
+    }
+}
+
+fn main() -> Result<(), String> {
+    let url = "https://httpbin.org/range/102400?duration=2";
+    const CHUNK_SIZE: u32 = 10240;
+
+    let client = reqwest::blocking::Client::new();
+    let response = client.head(url).send().unwrap();
+    let length = response
+        .headers()
+        .get(CONTENT_LENGTH)
+        .ok_or("Response doesn't include the CONTENT_LENGTH field")?;
+    let length = u64::from_str(length.to_str().unwrap()).map_err(|_| "Invalid content-length header").unwrap();
+
+    let mut output_file = File::create("download.bin").unwrap();
     
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    // println!("contents {:?}", contents);
+    println!("Starting download....");
 
-    let response = Client::new()
-        .post(paste_api)
-        .body(contents)
-        .send()
-        .await
-        .unwrap();
+    for range in PartialRangeIter::new(0, length - 1, CHUNK_SIZE).unwrap() {
+        println!("range: {:?}",range);
 
-    let response_text = response.text().await.unwrap();
-    println!("{:?}", response_text);  
-    println!("content length :{:?}", response.content_length());
+        let mut response = client.get(url).header(RANGE, range).send().unwrap();
+
+        let status = response.status();
+        if !(status == StatusCode::OK || status == StatusCode::PARTIAL_CONTENT) {
+            return Err(format!("Unexpected server response: {:?}", status));
+        }
+        std::io::copy(&mut response, &mut output_file).unwrap();
+    }
+
+    let mut content = response.text().unwrap();
+    std::io::copy(&mut content.as_bytes(), &mut output_file).unwrap();
+
+    println!("Finished with success");
 
     Ok(())
 }
