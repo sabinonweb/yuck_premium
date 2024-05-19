7d9a4b1c03f7ad3752079855ccc21aff483d9459
Sabin <sabinonweb7@gmail.com>
2024-05-19 20:45:01 +0545

    [Prelim: Async Download]

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
-use reqwest::header::{HeaderValue, CONTENT_LENGTH, RANGE};
-use std::fs::File;
-use reqwest::StatusCode;
-use std::str::FromStr;
+use reqwest::{header::{HeaderValue, CONTENT_LENGTH, RANGE}, Client, StatusCode};
+use std::{fs::File, str::FromStr};
+
+const YOUTUBE: &str = "https://www.youtube.com/watch?v=De97zQi5rzc&list=PLwooShOrg79yVGfA9OcjATf9EjEvi4bQu";
 
 #[derive(Debug)]
-struct PartialRangeIter {
+struct PartialRange {
     start: u64,
     end: u64,
     buffer: u32,
 }
 
-impl PartialRangeIter {
+impl PartialRange {
     fn new(start: u64, end: u64, buffer: u32) -> Result<Self, String> {
         if buffer == 0 {
-            Err(format!("Buffer must be greater than 0"))
+            Err(format!("Buffer should be greater than zero"))
         } else {
-            Ok(PartialRangeIter {
+            Ok(PartialRange {
                 start,
                 end,
                 buffer,
             })
         }
-    }
-}
+    } 
+} 
 
-impl Iterator for PartialRangeIter {
+impl Iterator for PartialRange {
     type Item = HeaderValue;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -33,43 +33,55 @@ impl Iterator for PartialRangeIter {
         } else {
             let prev_start = self.start;
             self.start += std::cmp::min(self.buffer as u64, self.end - self.start + 1);
-            Some(HeaderValue::from_str(&format!("bytes={}-{}", prev_start, self.start - 1)).expect("Expected &str, found String"))
+            Some(HeaderValue::from_str(&format!("Range={}-{}", prev_start, self.start - 1)).expect("Expected &str, found incompatible type"))
         }
-    }
+    } 
 }
 
-fn main() -> Result<(), String> {
-    let url = "https://httpbin.org/range/102400?duration=2";
+#[tokio::main]
+async fn main() -> Result<(), String> {
+    let url = "https://www.rust-lang.org/logos/rust-logo-16x16.png";
     const CHUNK_SIZE: u32 = 10240;
 
-    let client = reqwest::blocking::Client::new();
-    let response = client.head(url).send().unwrap();
+    let client = Client::new();
+    let response = client
+        .head(url)
+        .send()
+        .await
+        .unwrap();
+
     let length = response
         .headers()
         .get(CONTENT_LENGTH)
-        .ok_or("Response doesn't include the CONTENT_LENGTH field")?;
-    let length = u64::from_str(length.to_str().unwrap()).map_err(|_| "Invalid content-length header").unwrap();
+        .ok_or("content-length field not found in the response")
+        .unwrap();
 
-    let mut output_file = File::create("download.bin").unwrap();
-    
-    println!("Starting download....");
+    let length = u64::from_str(length.to_str().expect("Expected HeaderValue")).unwrap();
 
-    for range in PartialRangeIter::new(0, length - 1, CHUNK_SIZE).unwrap() {
-        println!("range: {:?}",range);
+    for range in PartialRange::new(0, length - 1, CHUNK_SIZE).unwrap() {
+        println!("Range: {:?}", range);
+        let mut response = client
+            .get(url)
+            .header(RANGE, range)
+            .send()
+            .await
+            .unwrap();
 
-        let mut response = client.get(url).header(RANGE, range).send().unwrap();
-
-        let status = response.status();
-        if !(status == StatusCode::OK || status == StatusCode::PARTIAL_CONTENT) {
-            return Err(format!("Unexpected server response: {:?}", status));
+        if !(response.status() == StatusCode::PARTIAL_CONTENT || response.status() == StatusCode::OK) {
+            return Err(format!("Unexpected server response: {:?}", response.status()));
         }
-        std::io::copy(&mut response, &mut output_file).unwrap();
+        let mut file = File::create("download.png").unwrap();
+        //std::io::copy(&mut response, &mut file);
+        //println!("Response: {:?}", response);
+
+        let mut response_text = response.text().await.unwrap();
+        std::io::copy(&mut response_text.as_bytes(), &mut file);
+
+        println!("finished");
+
+        println!("{:?}", std::cmp::min(10240, 10240));
     }
-
-    let mut content = response.text().unwrap();
-    std::io::copy(&mut content.as_bytes(), &mut output_file).unwrap();
-
-    println!("Finished with success");
-
     Ok(())
 }
+
+
